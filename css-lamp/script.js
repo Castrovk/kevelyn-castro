2f583476eb180f651c198e15e4848349619c7096
Castrovk <kevelync288@gmail.com>
2025-03-26 22:01:44 -0300

    projeto lampada

---
+++ css-lamp/script.js
@@ -0,0 +1,11 @@
+function turnLights() {
+    const lampada= document.getElementById ('bulb')
+    const interruptor= document.getElementById ('switch')
+
+lampada.classList.toggle('on');
+interruptor.classList.toggle('on')
+
+
+
+
+}
